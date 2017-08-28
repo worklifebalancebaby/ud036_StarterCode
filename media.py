4e9427503c0725c31943d911dd3de274b2cda37c
Dennis <31396952+worklifebalancebaby@users.noreply.github.com>
2017-08-28 22:53:27 +0900

    Update media.py

--- media.py
+++ media.py
@@ -2,10 +2,10 @@ import webbrowser
 
 
 '''This class initializes the Movie object with important attributes'''
- 
 class Movie():
     ''' the contructor method makes space for the attributes movie title, poster iamge url, and youtube trailer url'''
     def __init__(self, movie_title, poster, trailer):
         self.title = movie_title
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer
+ 
